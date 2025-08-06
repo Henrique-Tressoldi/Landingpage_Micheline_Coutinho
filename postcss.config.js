700d5b678df289bd3f3f918be634903e4b182953
Henrique-Tressoldi <tressoldih@gmail.com>
2025-08-06 19:31:44 -0300

    correcao tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,8 @@
-// Este é o arquivo de configuração do PostCSS. Ele informa ao Vercel
-// como processar o seu CSS para usar o Tailwind CSS e o Autoprefixer.
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  plugins: {
-    // A forma correta de referenciar o Tailwind CSS como um plugin do PostCSS
-    'tailwindcss': {},
-    // O Autoprefixer adiciona prefixos de fornecedor para garantir a compatibilidade do navegador
-    'autoprefixer': {},
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {},
   },
-};
+  plugins: [],
+}
